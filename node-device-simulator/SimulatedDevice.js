53c1dc240f007b0e9602f2be35efc0ecc439b1a6
Ryan Joy <ryanjoy@microsoft.com>
2016-06-22 14:55:30 -0700

    Node console script to read from CSV and simulate data transmission

---
+++ node-device-simulator/SimulatedDevice.js
@@ -0,0 +1,57 @@
+'use strict';
+var fs = require('fs'), 
+    parse = require('csv-parse'),
+    nconf = require('nconf');
+
+    // Create nconf environtment 
+    nconf 
+        .file({ file: './config.json' }) 
+        .env(); 
+
+var connectionString = nconf.get("connectionString");
+
+var clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
+var Message = require('azure-iot-device').Message;
+
+var client = clientFromConnectionString(connectionString);
+
+function printResultFor(op) {
+    return function printResult(err, res) {
+        if (err) console.log(op + ' error: ' + err.toString());
+        if (res) console.log(op + ' status: ' + res.constructor.name);
+    };
+}
+
+var connectCallback = function (err) {
+    if (err) {
+        console.log('Could not connect: ' + err);
+    } else {
+        console.log('Client connected');
+
+        var eventName = nconf.get("eventName")
+        var parser = parse({delimiter: ',', columns: true}, function(err, data){
+            
+            var x = 0;
+            var interval = setInterval( function (){
+
+                data[x]["eventName"] = eventName;
+
+                var message = new Message(JSON.stringify(data[x]));
+                
+                // Send device message to IoT Hub client
+                client.sendEvent(message, printResultFor('send'));
+
+                x++;
+                if (x == data.length) clearInterval(interval);
+                
+
+            }, 100);
+
+        });
+
+        fs.createReadStream('Results_10000201.csv').pipe(parser);
+
+  }
+};
+
+client.open(connectCallback);
